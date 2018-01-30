c555b616eda651e3b5defdd628b35e9d35651d46
Simon Adcock <simon.adcock@guardian.co.uk>
2018-01-30 17:23:27 +0000

    script refactor

--- build.sh
+++ build.sh
@@ -18,34 +18,31 @@ if [[ -z $BRANCH_NAME ]]; then
   BRANCH_NAME=unknown
 fi
 
+npm install
+cd lambda
+zip -r lambda.zip render.js ../package.json ../node_modules/
+cd ..
+[ -d target ] && rm -rf target
+mkdir -p target/lambda
+mkdir -p target/cfn
+mv lambda/lambda.zip ./target/lambda/lambda.zip
+cp cloudformation.yml ./target/cfn
+
+cp riff-raff.yaml target
+
 
 BUILD_START_DATE=$(date +"%Y-%m-%dT%H:%M:%S.000Z")
 
-cat >build.json << EOF
+cat >target/build.json << EOF
 {
    "projectName":"$PROJECT",
    "buildNumber":"$BUILD_NUMBER",
    "startTime":"$BUILD_START_DATE",
    "revision":"$BUILD_VCS_NUMBER",
-   "vcsURL":"git@github.com:guardian/frontend-lambda.git",
+   "vcsURL":"git@github.com:guardian/moon.git",
    "branch":"$BRANCH_NAME"
 }
 EOF
-
-cd lambda
-npm install
-zip -r moonLambda.zip render.js ../package.json ../node_modules/
-cd ..
-[ -d target ] && rm -rf target
-mkdir target
-cd target
-mkdir -p moonLambda
-mkdir -p cfn
-cp ../lambda/moonLambda.zip ./moonLambda/moonLambda.zip
-cp ../cloudformation.yml ./cfn
-
-cp ../riff-raff.yaml .
-cd ..
 
 aws s3 cp --acl bucket-owner-full-control --region=eu-west-1 --recursive target s3://riffraff-artifact/$PROJECT/$BUILD_NUMBER
 aws s3 cp --acl bucket-owner-full-control --region=eu-west-1 build.json s3://riffraff-builds/$PROJECT/$BUILD_NUMBER/build.json
