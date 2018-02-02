c31d6584a91dc4e8eecc36b24dd77789b535d6db
Simon Adcock <simon.adcock@guardian.co.uk>
2018-02-02 16:41:41 +0000

    simplify

--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
-const fs = require('fs');
-const pify = require('pify');
-
-const readFile = pify(fs.readFile);
+const html = require('./not-found');
 
 module.exports = function(req, res) {
     if (req.url === '/healthcheck') {
         res.end('OK');
     } else {
-        readFile(`${__dirname}/not-found.html`, 'utf-8')
-            .then(data => {
-                res.end(data.toString());
-            })
-            .catch(err => {
-                console.log(err);
-                res.end(err.toString());
-            });
+        res.end(html);
     }
 };
