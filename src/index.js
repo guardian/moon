2944bbfa18c33acdb1821153ed2778401432fae1
Simon Adcock <simon.adcock@guardian.co.uk>
2018-02-02 12:05:59 +0000

    add healthcheck

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ const pify = require('pify');
 const readFile = pify(fs.readFile);
 
 module.exports = function(req, res) {
-    readFile(`${__dirname}/not-found.html`, 'utf-8')
-        .then(data => {
-            res.end(data.toString());
-        })
-        .catch(err => {
-            console.log(err);
-            res.end(err.toString());
-        });
+    if (req.url === '/healthcheck') {
+        res.end('OK');
+    } else {
+        readFile(`${__dirname}/not-found.html`, 'utf-8')
+            .then(data => {
+                res.end(data.toString());
+            })
+            .catch(err => {
+                console.log(err);
+                res.end(err.toString());
+            });
+    }
 };
